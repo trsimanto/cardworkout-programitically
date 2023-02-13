1235b6f848adb108185d7e73edfb0d01a4ca9c45
trsimanto <simanto14feb@gmail.com>
2023-02-13 18:43:05 +0600

    init

---
+++ CardWorkout-ProgrammaticUI/RulesVC.swift
@@ -0,0 +1,24 @@
+//
+//  RulesVC.swift
+//  CardWorkout-ProgrammaticUI
+//
+//  Created by REEA on 13/2/23.
+//
+
+import UIKit
+
+class RulesVC: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        configUI()
+    }
+    func configUI(){
+     
+    }
+    
+
+
+}
