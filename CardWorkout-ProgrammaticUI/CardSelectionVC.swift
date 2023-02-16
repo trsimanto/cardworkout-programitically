36bac82e7874215fef2c99b0a02a2dea86313a55
trsimanto <simanto14feb@gmail.com>
2023-02-16 15:58:11 +0600

    rules target added

--- CardWorkout-ProgrammaticUI/CardSelectionVC.swift
+++ CardWorkout-ProgrammaticUI/CardSelectionVC.swift
@@ -67,7 +67,7 @@ class CardSelectionVC: UIViewController {
     
     func configureRulesButton(){
         view.addSubview(rulesButton)
-        
+        rulesButton.addTarget(self, action: #selector(presentRulesVC), for: .touchUpInside)
         NSLayoutConstraint.activate([
             rulesButton.widthAnchor.constraint(equalToConstant: 115),
             rulesButton.heightAnchor.constraint(equalToConstant: 50),
@@ -76,5 +76,10 @@ class CardSelectionVC: UIViewController {
             
         ])
     }
+    
+    @objc func presentRulesVC(){
+        present(RulesVC(), animated: true)
+    }
+    
     
 }
